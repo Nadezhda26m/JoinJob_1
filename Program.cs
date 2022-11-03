adf8fbc514e549045216563ec074e7e619132f1f
Nadezhda26m <nadezhda-26m@mail.ru>
2022-11-03 10:39:37 +0300

    Method: Print2ArrayStrings

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 // 1. Метод создания массива строк
 // 2. Метод заполнения массива строк случайными символами
 // 3. Метод печати массива строк
+// 4. Метод печати двух массивов одинаковой длины рядом
 
 // 1. Метод создания массива строк
 string[] CreateArrayStrings(int size)
@@ -32,5 +33,15 @@ void PrintArrayStrings(string[] text)
     for (int i = 0; i < size; i++)
     {
         Console.WriteLine(text[i]);
+    }
+}
+
+// 4. Метод печати двух массивов одинаковой длины рядом
+void Print2ArrayStrings(string[] text, string[] text2)
+{
+    int size = text.Length;
+    for (int i = 0; i < size; i++)
+    {
+        Console.WriteLine($"{text[i],15}{text2[i],19}");
     }
 }
