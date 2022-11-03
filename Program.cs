b6b84533bd9b6e493b3fd956a5555f1c00c4393d
Nadezhda26m <nadezhda-26m@mail.ru>
2022-11-03 10:40:31 +0300

    Method: RemoveNumbersFromArrayStrings

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@
 // 2. Метод заполнения массива строк случайными символами
 // 3. Метод печати массива строк
 // 4. Метод печати двух массивов одинаковой длины рядом
+// 5. Метод удаления из массива строк цифр
 
 // 1. Метод создания массива строк
 string[] CreateArrayStrings(int size)
@@ -44,4 +45,24 @@ void Print2ArrayStrings(string[] text, string[] text2)
     {
         Console.WriteLine($"{text[i],15}{text2[i],19}");
     }
+}
+
+// 5. Метод удаления из массива строк цифр
+string[] RemoveNumbersFromArrayStrings(string[] text)
+{
+    int size = text.Length;
+    string[] result = new string[size];
+    int lengthItem = 0;
+    for (int i = 0; i < size; i++)
+    {
+        lengthItem = text[i].Length;
+        for (int j = 0; j < lengthItem; j++)
+        {
+            if (!(text[i][j] >= '0' && text[i][j] <= '9'))
+            {
+                result[i] += text[i][j];
+            }
+        }
+    }
+    return result;
 }
